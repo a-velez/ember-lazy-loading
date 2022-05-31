09c370eda0c9663d4f9c1d1b1691c1a622fc3afc
avelez <avelez@copyright.com>
2022-05-31 11:34:00 +0200

    folders content added

---
+++ ember-app/app/components/lazy.js
@@ -0,0 +1,20 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+
+export default class LazyComponent extends Component {
+  @tracked TheComponent;
+  @tracked error;
+  loadComponent = async () => {
+    this.error = null;
+
+    try {
+      console.log(this.args.path);
+      // let importedModule = await import('ember-addon/components/demo');
+      let importedModule = await import(this.args.path);
+
+      this.TheComponent = importedModule.default;
+    } catch (e) {
+      this.error = e;
+    }
+  };
+}
